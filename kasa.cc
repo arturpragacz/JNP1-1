ffc45990452175135481600a20ce6e3a88b70854
Artur Pragacz <arturpragacz@gmail.com>
2019-10-15 22:37:04 +0200

    parseJourney

--- kasa.cc
+++ kasa.cc
@@ -32,9 +32,46 @@ namespace {
 	using Route = std::unordered_map<std::string, Stop>; // key = name of the stop
 	using Routes = std::unordered_map<unsigned int, Route>; // key = number of the route
 
-	void parseJourney(const std::string& line, std::vector<std::string>& stopNames, std::vector<int>& routeNumbers) {
+	class Patterns {
+	private:
+		static const std::string stopNamePattern;
+		static const std::string RouteNumberPattern;
 
-	}
+	public:
+		bool parseJourney(const std::string& line, std::vector<std::string>& stopNames, std::vector<int>& routeNumbers) {
+			static const std::string initialPattern = "? ";
+			if (line.substr(0, 2) != initialPattern)
+				return false;
+
+			static const std::string routeNumberOrNothingPattern = "(?:$| " + RouteNumberPattern + ")";
+			static const std::string segmentPattern = "^" + stopNamePattern + routeNumberOrNothingPattern;
+			static const std::regex segmentRegex(segmentPattern);
+
+			std::smatch segmentMatch;
+			for (auto it = line.begin() + 2; it != line.end(); ) {
+				if (std::regex_search(it, line.end(), segmentMatch, segmentRegex)) {
+					stopNames.push_back(segmentMatch[1]);
+					if (segmentMatch.size() == 3) {
+						try {
+							routeNumbers.push_back(stoi(segmentMatch[2]));
+						}
+						catch (std::out_of_range& e) {
+							return false;
+						}
+					}
+					it = segmentMatch.suffix().first;
+				}
+				else
+					return false;
+			}
+
+			return true;
+		}
+
+	} patterns;
+
+	const std::string Patterns::stopNamePattern = "([A-Za-z_^]+)";
+	const std::string Patterns::RouteNumberPattern = "0*([0-9]+)";
 
 	int computeTimeNeededForJourney(const Routes& routes, const std::vector<std::string>& stopNames,
 					const std::vector<int>& routeNumbers) {
@@ -67,9 +104,12 @@ namespace {
 	int findTickets(const std::string& line, const Routes& routes, tickets) {
 		std::vector<std::string> stopNames;
 		std::vector<int> routeNumbers;
-		parseJourney(line, stopNames, routeNumbers);
+		if (!patterns.parseJourney(line, stopNames, routeNumbers))
+			return -1;
 
 		int timeNeeded = computeTimeNeededForJourney(routes, stopNames, routeNumbers);
+		if (timeNeeded == -1)
+			return -1;
 
 		findCheapestTickets(timeNeeded);
 	}
@@ -94,7 +134,8 @@ int main() {
 				break;
 
 			case question:
-				findTickets(line, routes, tickets);
+				if (findTickets(line, routes, tickets))
+					printError(i, line);
 				break;
 
 			case incorrect:
