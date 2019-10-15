a5a2d336e7f31dc350f291b505e417e31216ad78
Artur Pragacz <arturpragacz@gmail.com>
2019-10-15 15:05:40 +0200

    computeTimeNeededForJourney

--- kasa.cc
+++ kasa.cc
@@ -32,10 +32,46 @@ namespace {
 	using Route = std::unordered_map<std::string, Stop>; // key = name of the stop
 	using Routes = std::unordered_map<unsigned int, Route>; // key = number of the route
 
-	void findTickets(const std::string& line, const Routes& routes, tickets) {
-		parseJourney();
-		int timeNeeded = computeTimeNeededForJourney();
+	void parseJourney(const std::string& line, std::vector<std::string>& stopNames, std::vector<int>& routeNumbers) {
 
+	}
+
+	int computeTimeNeededForJourney(const Routes& routes, const std::vector<std::string>& stopNames,
+					const std::vector<int>& routeNumbers) {
+		int timeNeeded = 0;
+		for (size_t i = 0; i < routeNumbers.size(); ++i) {
+			try {
+				Route route = routes.at(routeNumbers[i]);
+
+				Stop stop1 = route.at(stopNames[i]);
+				Stop stop2 = route.at(stopNames[i + 1]);
+
+				int order1 = stop1.first;
+				int order2 = stop2.first;
+				int time1 = stop1.second;
+				int time2 = stop2.second;
+
+				if (order1 >= order2)
+					return -1;
+				else
+					timeNeeded += time2 - time1;
+			}
+			catch (std::out_of_range& e) {
+				return -1;
+			}
+		}
+
+		return timeNeeded;
+	}
+
+	int findTickets(const std::string& line, const Routes& routes, tickets) {
+		std::vector<std::string> stopNames;
+		std::vector<int> routeNumbers;
+		parseJourney(line, stopNames, routeNumbers);
+
+		int timeNeeded = computeTimeNeededForJourney(routes, stopNames, routeNumbers);
+
+		findCheapestTickets(timeNeeded);
 	}
 
 }
